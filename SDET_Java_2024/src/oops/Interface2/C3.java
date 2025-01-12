33d5a40b7d88f6889b529d53de365f874c977742
vivek <vivek.sirsath1@gmail.com>
2025-01-12 23:37:09 +0530

    oops.Interface2 package - C3 class added

---
+++ SDET_Java_2024/src/oops/Interface2/C3.java
@@ -0,0 +1,10 @@
+package oops.Interface2;
+
+public class C3 {
+	
+	int z = 300; // static and final variable
+	void m3() { // NOT ABSTRACT but Normal Method
+		System.out.println("value of z= " + z);
+	} 
+
+}
