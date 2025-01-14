b7bbdc7ab18cadc88d90ede81c7e86288c5e6b68
vivek <vivek.sirsath1@gmail.com>
2025-01-14 23:57:08 +0530

    ThisKeyword3 added

---
+++ SDET_Java_2024/src/thisAndStaticKeywords/ThisKeyword3.java
@@ -0,0 +1,43 @@
+// Session 13
+/* If we don't use this keyword, compiler will not understand
+ * to which variable it should assign the values, whether to local variables 
+ * inside method or to instance variables declared at class level.
+ * - If we run the program, compiler will show the default value of integer.
+ *   i.e. 
+ *        x = 0 , y = 0
+*/
+package thisAndStaticKeywords;
+
+public class ThisKeyword3 {
+
+	int x, y; // instance variable
+/*
+	public ThisKeyword1(int x, int y) { // local variable in Parameterized Constructor
+		x = x;  // Not used this keyword
+		y = y;  // Not used this keyword
+	}
+*/	
+	void setData(int a, int b) { // local variable in User Defined Method
+		x = a;
+		y = b;
+	}
+
+	void display() {
+		System.out.println(x); // 0
+		System.out.println(y); // 0
+	}
+
+	public static void main(String[] args) {
+		
+// 1) In case of constructor, no need to create object of class		
+//		ThisKeyword1 th = new ThisKeyword1(15, 25);
+//		th.display();
+		
+// 2) In case of method, need to create object of class		
+		ThisKeyword3 th = new ThisKeyword3();
+		th.setData(100, 200);
+		th.display();
+
+	}
+
+}
