ceaf6e5f7968d69c85c8d49b42aced9befae643b
vivek <vivek.sirsath1@gmail.com>
2025-01-17 22:40:24 +0530

    typeCasting package added

---
+++ SDET_Java_2024/src/typeCasting/TypeCastingForPrimitiveDataTypes.java
@@ -0,0 +1,44 @@
+package typeCasting;
+
+public class TypeCastingForPrimitiveDataTypes {
+
+	public static void main(String[] args) {
+
+		// UpCasting - Automatic --> Smaller to Larger
+
+		int intValue = 100;
+		long longValue = intValue;
+		System.out.println(longValue); // 100
+
+		float floatValue = 22.5F; // F is a literal
+		double doubleValue = floatValue; // float is smaller than double
+		System.out.println(doubleValue); // 22.5
+
+		// DownCasting - Manual --> Larger to Smaller
+
+		long longValue1 = 9990;
+		int intValue1 = (int) longValue1;
+		System.out.println(intValue1); // 9990
+		// Here we are converting larger value in to smaller value manually.
+		// Then assigning to smaller value.
+
+		double doubleValue1 = 125.55;
+		float floatValue1 = (float) doubleValue1;
+		System.out.println(floatValue1);
+
+		// Integer to Decimal
+		int i = 355;
+		double d = i;
+		System.out.println(d); // 355.0
+
+		// Decimal to Integer
+		// There is problem with DownCasting,
+		// we can't convert the actual decimal value into integer value
+		double d1 = 86.3;
+		int i1 = (int) d1;
+		System.out.println(i1); // 86 instead of 86.3
+		// Here, 86 is printed instead of 86.3, Not printing actual value
+
+	}
+
+}
