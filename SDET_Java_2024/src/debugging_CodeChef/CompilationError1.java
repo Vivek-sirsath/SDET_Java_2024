8a50ca920bbc3420999d04fe8389fd9ce9060bb1
vivek <vivek.sirsath1@gmail.com>
2025-03-05 22:51:37 +0530

    CompileTimeError 1 and 2 programs added

---
+++ SDET_Java_2024/src/debugging_CodeChef/CompilationError1.java
@@ -0,0 +1,17 @@
+/*
+// Compilation error / Syntax error
+
+package debugging_CodeChef;
+
+class CompilationError1{
+	
+    public static void main(String[] args) {
+    
+        Int a = 3; // change capital I to small i
+        Int b = 5;
+        
+        System.out.println(a * b)   // add semicolon
+    }
+}
+
+*/
---
+++ SDET_Java_2024/src/debugging_CodeChef/CompilationError2.java
@@ -0,0 +1,19 @@
+/*
+// If we try to use different opening and closing brackets, it can give you a syntax error.
+ 
+package debugging_CodeChef;
+
+public class CompilationError2 {
+	
+    public static void main(String[] args) {
+        int n;
+        n = 5;
+        if (n % 2 == 1) {
+            System.out.println("Odd");
+        } else {
+            System.out.println("Even");
+        )     // correct the closing bracket
+    }
+}
+
+*/
