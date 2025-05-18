0f7db8e406399e3447e918dbbf0ec0fafa3d4d65
vivek <vivek.sirsath1@gmail.com>
2025-05-18 21:51:45 +0530

    Comment modified

--- SDET_Java_2024/src/polymorphism_Static_MethodOverloading/MethodOverload6.java
+++ SDET_Java_2024/src/polymorphism_Static_MethodOverloading/MethodOverload6.java
@@ -8,7 +8,7 @@ public class MethodOverload6 {
 		System.out.println(MethodOverload5.add(10.5, 5.5));  // 16.0
 		System.out.println(MethodOverload3.add(20, 10, 10));  // 40
 		/* Here we can call 'MethodOverload3' class methods because it is static
-		   we can call static methods from one class to another class by the help of that class name.
+		   In the same package, we can call static methods from one class to another class by the help of that class name.
 		   But those classes should be in same package.  
 		*/
 		
