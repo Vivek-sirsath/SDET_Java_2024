288a2933f04b9bb9a6c1249d8308b16063364fed
vivek <vivek.sirsath1@gmail.com>
2025-05-18 21:50:42 +0530

    add method called using MethodOverload3 reference

--- SDET_Java_2024/src/polymorphism_Static_MethodOverloading/MethodOverload4.java
+++ SDET_Java_2024/src/polymorphism_Static_MethodOverloading/MethodOverload4.java
@@ -7,6 +7,9 @@ public class MethodOverload4 extends MethodOverload3 {
 		System.out.println(MethodOverload4.add(10, 5)); // 15
 		System.out.println(MethodOverload4.add(20, 10, 10)); // 40
 		
+		System.out.println(MethodOverload3.add(10, 5)); // 15
+		System.out.println(MethodOverload3.add(20, 10, 10)); // 40
+		
 		// Here we can use both 'MethodOverload3' as well as 'MethodOverload4'
 
 	}
