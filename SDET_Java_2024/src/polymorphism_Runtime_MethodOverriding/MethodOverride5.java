3296603c6d4d76638bb54aad37a367dfbe9f046e
vivek <vivek.sirsath1@gmail.com>
2025-05-23 20:32:18 +0530

    MethodOverride5 added

---
+++ SDET_Java_2024/src/polymorphism_Runtime_MethodOverriding/MethodOverride5.java
@@ -0,0 +1,16 @@
+package polymorphism_Runtime_MethodOverriding;
+
+public class MethodOverride5 {
+
+	public void cook() {
+		System.out.println("Mom is cooking");
+	}
+	
+	public void eat() {
+		System.out.println("I am eating");
+	}
+	
+	public void handwash() {
+		System.out.println("Daily handwash");
+	}
+}
