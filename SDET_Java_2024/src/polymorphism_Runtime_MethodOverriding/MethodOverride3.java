9f51b1d5433c4abe8098e0811b8d5f7ec6e783f3
vivek <vivek.sirsath1@gmail.com>
2025-05-23 20:31:51 +0530

    MethodOverride 3 and 4 added

---
+++ SDET_Java_2024/src/polymorphism_Runtime_MethodOverriding/MethodOverride3.java
@@ -0,0 +1,8 @@
+package polymorphism_Runtime_MethodOverriding;
+
+public class MethodOverride3 {
+
+	public void run() {
+		System.out.println("I am running");
+	}
+}
---
+++ SDET_Java_2024/src/polymorphism_Runtime_MethodOverriding/MethodOverride4.java
@@ -0,0 +1,19 @@
+package polymorphism_Runtime_MethodOverriding;
+
+public class MethodOverride4 extends MethodOverride3 {
+	
+	public void run() {
+		System.out.println("You are running");
+	}
+
+	public static void main(String[] args) {
+		
+		MethodOverride4 mo4 = new MethodOverride4();
+		mo4.run();  // You are running
+
+		MethodOverride3 mo3 = new MethodOverride3();
+		mo3.run();  // I am running
+
+	}
+
+}
